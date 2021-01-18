947728dd450b7f54a2838c67959069bb8261a4c1
Diego Ignacio Nuñez Hernandez <diegodog2312sam@gmail.com>
2021-01-18 14:20:58 -0600

    vvff

--- Programas/dijkstra.c
+++ Programas/dijkstra.c
@@ -21,14 +21,11 @@ int nv,            // number of vertices
     md,            // current min over all threads
     mv,            // vertex which achieves that min
     largeint = -1; // max possible unsigned int
+unsigned *ohd, // 1-hop distances between vertices; "ohd[i][j]" is ohd[i*nv+j]
+        *mind; // min distances found so far
 
-unsigned *ohd, // 1-hop distances between vertices; "ohd[i][j]" is
-    // ohd[i*nv+j]
-    *mind; // min distances found so far
-
-void init(int ac, char **av)
-{
-    omp_set_num_threads(1);
+void init(int ac, char **av){
+    omp_set_num_threads(4);
     srand(2);
     int i, j, tmp;
     nv = atoi(av[1]);
@@ -37,82 +34,71 @@ void init(int ac, char **av)
     notdone = malloc(nv * sizeof(int));
     // random graph
     for (i = 0; i < nv; i++)
-        for (j = i; j < nv; j++)
-        {
+        for (j = i; j < nv; j++){
             if (j == i)
                 ohd[i * nv + i] = 0;
-            else
-            {
+            else{
                 ohd[nv * i + j] = rand() % 20;     // Llena el grafo con numeros aleatorios
                 ohd[nv * j + i] = ohd[nv * i + j]; //El valor de ida es el mismo que de regreso
             }
         }
-    for (i = 1; i < nv; i++)
-    {
+    for (i = 1; i < nv; i++){
         notdone[i] = 1;   //Ningun verrtice está revisado aún
         mind[i] = ohd[i]; //La distancia mínima encontrada hasta ahora es
     }
-
     //sobre escribe el 15 al nodo e
     ohd[nv * 0 + 4] = 30;
     ohd[4 * nv] = 30;
 }
 
-void dowork()
-{
-
-#pragma omp parallel
+void dowork(){
+    #pragma omp parallel
     {
         int step, // whole procedure goes nv steps
             mymv;  // vertex which attains that value
-
         unsigned mymd; // min value found by this thread
 
-        for (step = 0; step < nv; step++)
-        {
-
-// closest in its group, then we find overall closest
-#pragma omp single
+        for (step = 0; step < nv; step++){
+            // closest in its group, then we find overall closest
+            #pragma omp single
             {
                 md = largeint;
                 mv = 0;
             }
+            
             mymd = largeint;
-#pragma omp for nowait
-            for (int i = 1; i < nv; i++)
-            {
-                if (notdone[i] && mind[i] < mymd)
-                {
+            
+            #pragma omp for nowait
+            for (int i = 1; i < nv; i++){
+                if (notdone[i] && mind[i] < mymd){
                     mymd = ohd[i];
                     mymv = i;
                 }
             }
-// update overall min if mine is smaller
-#pragma omp critical
+            // update overall min if mine is smaller
+            #pragma omp critical
             {
-                if (mymd < md)
-                {
+                if (mymd < md){
                     md = mymd;
                     mv = mymv;
                 }
             }
-#pragma omp barrier
-// mark new vertex as done
-#pragma omp single
+            #pragma omp barrier
+            // mark new vertex as done
+            #pragma omp single
             {
                 notdone[mv] = 0;
             }
-// now update ohd
-#pragma omp for
-            for (int i = 1; i < nv; i++)
-                if (mind[mv] + ohd[mv * nv + i] < mind[i])
-                    mind[i] = mind[mv] + ohd[mv * nv + i];
+            // now update ohd
+            #pragma omp for
+                for (int i = 1; i < nv; i++)
+                    if (mind[mv] + ohd[mv * nv + i] < mind[i])
+                        mind[i] = mind[mv] + ohd[mv * nv + i];
         }
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     int i, j, print;
     double startime, endtime;
     init(argc, argv);
@@ -122,11 +108,9 @@ int main(int argc, char **argv)
     // back to single thread
     endtime = omp_get_wtime();
     print = atoi(argv[2]);
-    if (print)
-    {
+    if (print){
         printf("graph weights:\n");
-        for (i = 0; i < nv; i++)
-        {
+        for (i = 0; i < nv; i++){
             for (j = 0; j < nv; j++)
                 printf("%u ", ohd[nv * i + j]);
             printf("\n");
@@ -134,7 +118,6 @@ int main(int argc, char **argv)
         printf("minimum distances:\n");
         for (i = 1; i < nv; i++)
             printf("%u\n", mind[i]);
-
         printf("\n\n");
     }
     printf("Tiempo de ejecucion: %f", endtime - startime);
--- Programas/dijkstra2.c
+++ Programas/dijkstra2.c
@@ -16,12 +16,10 @@ int nv,            // number of vertices
     mv,            // vertex which achieves that min
     largeint = -1; // max possible unsigned int
 
-unsigned *ohd, // 1-hop distances between vertices; "ohd[i][j]" is
-    // ohd[i*nv+j]
-    *mind; // min distances found so far
+unsigned *ohd, // 1-hop distances between vertices; "ohd[i][j]" is ohd[i*nv+j]
+        *mind; // min distances found so far
 
-void init(int ac, char **av)
-{
+void init(int ac, char **av){
     srand(2);
     int i, j, tmp;
     nv = atoi(av[1]);
@@ -30,18 +28,15 @@ void init(int ac, char **av)
     notdone = malloc(nv * sizeof(int));
     // random graph
     for (i = 0; i < nv; i++)
-        for (j = i; j < nv; j++)
-        {
+        for (j = i; j < nv; j++){
             if (j == i)
                 ohd[i * nv + i] = 0;
-            else
-            {
+            else{
                 ohd[nv * i + j] = rand() % 20;     // Llena el grafo con numeros aleatorios
                 ohd[nv * j + i] = ohd[nv * i + j]; //El valor de ida es el mismo que de regreso
             }
         }
-    for (i = 1; i < nv; i++)
-    {
+    for (i = 1; i < nv; i++){
         notdone[i] = 1;   //Ningun verrtice está revisado aún
         mind[i] = ohd[i]; //La distancia mínima encontrada hasta ahora es
     }
@@ -54,23 +49,19 @@ void dowork()
         i;
     unsigned mymd; // min value found by this thread
 
-    for (step = 0; step < nv; step++)
-    {
+    for (step = 0; step < nv; step++){
         // closest in its group, then we find overall closest
         md = largeint;
         mv = 0;
         mymd = largeint;
-        for (i = 1; i < nv; i++)
-        {
-            if (notdone[i] && mind[i] < mymd)
-            {
+        for (i = 1; i < nv; i++){
+            if (notdone[i] && mind[i] < mymd){
                 mymd = ohd[i];
                 mymv = i;
             }            
         }
         // update overall min if mine is smaller
-        if (mymd < md)
-        {
+        if (mymd < md){
             md = mymd;
             mv = mymv;
         }
@@ -83,8 +74,7 @@ void dowork()
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     int i, j, print;
     double startime, endtime;
     init(argc, argv);
@@ -92,11 +82,9 @@ int main(int argc, char **argv)
     dowork();
     endtime = omp_get_wtime();
     print = atoi(argv[2]);
-    if (print)
-    {
+    if (print){
         printf("graph weights:\n");
-        for (i = 0; i < nv; i++)
-        {
+        for (i = 0; i < nv; i++){
             for (j = 0; j < nv; j++)
                 printf("%u ", ohd[nv * i + j]);
             printf("\n");
